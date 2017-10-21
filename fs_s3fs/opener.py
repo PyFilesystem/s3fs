87ecfe43165b9de0c44f5a4493892e0506eb43bc
Will McGugan <willmcgugan@gmail.com>
2017-10-21 16:12:26 +0100

    fix for strict in opener

--- fs_s3fs/opener.py
+++ fs_s3fs/opener.py
@@ -22,12 +22,17 @@ class S3FSOpener(Opener):
             raise OpenerError(
                 "invalid bucket name in '{}'".format(fs_url)
             )
-        strict = parse_result.params.get('strict', 'y') == 'y'
+        strict = (
+            parse_result.params['strict'] == '1'
+            if 'strict' in parse_result.params
+            else True
+        )
         s3fs = S3FS(
             bucket_name,
             dir_path=dir_path or '/',
             aws_access_key_id=parse_result.username or None,
             aws_secret_access_key=parse_result.password or None,
-            endpoint_url=parse_result.params.get('endpoint_url', None)
+            endpoint_url=parse_result.params.get('endpoint_url', None),
+            strict=strict
         )
         return s3fs
