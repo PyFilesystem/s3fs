f11f240441ba6401ffbb4996998ff59d1753c2df
Bradley McElroy <bradley.mcelroy@imc.com>
2018-02-19 09:44:57 +1100

    Fix removedir breaking on last file remove

--- fs_s3fs/tests/test_s3fs.py
+++ fs_s3fs/tests/test_s3fs.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+import os
 import unittest
 
 from nose.plugins.attrib import attr
@@ -69,3 +70,25 @@ class TestS3FSHelpers(unittest.TestCase):
         self.assertEqual(s3._path_to_key('foo.bar'), 'dir/foo.bar')
         self.assertEqual(s3._path_to_key('foo/bar'), 'dir/foo/bar')
 
+
+class TestRemoveTree(unittest.TestCase):
+
+    bucket_name = 'fsexample'
+
+    def test_removetree_multilevel(self):
+        end_point = 'http://' + os.environ.get('S3_ENDPOINT') if 'S3_ENDPOINT' in os.environ else None
+        session = boto3.session.Session()
+        client = session.client(
+            service_name='s3',
+            endpoint_url=end_point
+        )
+        if self.bucket_name not in [b['Name'] for b in client.list_buckets()['Buckets']]:
+            client.create_bucket(Bucket=self.bucket_name)
+
+        fs = S3FS(bucket_name=self.bucket_name, endpoint_url=end_point)
+
+        dr = fs.makedirs('data/hello')
+        with dr.openbin('world', 'w') as f:
+            f.write(b'somedata')
+
+        fs.removetree('data')
