1c5e3a1b6abbb9dff91ea7fc4cec7353798cd536
Will McGugan <willmcgugan@gmail.com>
2019-01-01 17:35:45 +0000

    bump to fs 2.2.0 (#48)
    * bump to fs 2.2.0
    
    * version bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,15 @@
 # Change Log
+
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [1.1.0] - 2018-01-01
+
+### Changed
+
+- Bumped FS to 2.2.0
 
 ## [1.0.0] - 2018-08-17
 
--- setup.py
+++ setup.py
@@ -2,35 +2,31 @@
 
 from setuptools import setup, find_packages
 
-with open('fs_s3fs/_version.py') as f:
+with open("fs_s3fs/_version.py") as f:
     exec(f.read())
 
 CLASSIFIERS = [
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Topic :: System :: Filesystems',
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Topic :: System :: Filesystems",
 ]
 
-with open('README.rst', 'rt') as f:
+with open("README.rst", "rt") as f:
     DESCRIPTION = f.read()
 
-REQUIREMENTS = [
-    "boto3~=1.7",
-    "fs~=2.1",
-    "six~=1.10"
-]
+REQUIREMENTS = ["boto3~=1.7", "fs~=2.2", "six~=1.10"]
 
 setup(
-    name='fs-s3fs',
+    name="fs-s3fs",
     author="Will McGugan",
     author_email="willmcgugan@gmail.com",
     classifiers=CLASSIFIERS,
@@ -39,12 +35,10 @@ setup(
     license="MIT",
     long_description=DESCRIPTION,
     packages=find_packages(),
-    keywords=['pyfilesystem', 'Amazon', 's3'],
-    platforms=['any'],
+    keywords=["pyfilesystem", "Amazon", "s3"],
+    platforms=["any"],
     test_suite="nose.collector",
     url="https://github.com/PyFilesystem/s3fs",
     version=__version__,
-    entry_points={'fs.opener': [
-         's3 = fs_s3fs.opener:S3FSOpener',
-    ]},
+    entry_points={"fs.opener": ["s3 = fs_s3fs.opener:S3FSOpener"]},
 )
