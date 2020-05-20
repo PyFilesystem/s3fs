4878610b0f6615f071a757c42857eee6afe23d1d
Antoine Sinton <antoine@cnty.ai>
2020-05-20 16:07:33 +0200

    remove obj.load in getinfo

--- fs_s3fs/_s3fs.py
+++ fs_s3fs/_s3fs.py
@@ -436,7 +436,6 @@ class S3FS(FS):
                 _dir_key = self._path_to_dir_key(dir_path)
                 with s3errors(path):
                     obj = self.s3.Object(self._bucket_name, _dir_key)
-                    obj.load()
         except errors.ResourceNotFound:
             raise errors.ResourceNotFound(path)
 
